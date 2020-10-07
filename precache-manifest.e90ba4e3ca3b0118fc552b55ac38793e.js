191dd969ff1a69639244d1908c99109efc3b86ae
ScotyRU <scotyrus@gmail.com>
2020-10-07 12:50:28 +0500

    Updates

---
+++ precache-manifest.e90ba4e3ca3b0118fc552b55ac38793e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf12d9ea565f73edc8f40c0207fdc18d",
+    "url": "/tools/index.html"
+  },
+  {
+    "revision": "62208d0b4c8b9d21ba14",
+    "url": "/tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73b4f8a35333eb08eb47",
+    "url": "/tools/static/js/2.a0c979ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tools/static/js/2.a0c979ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62208d0b4c8b9d21ba14",
+    "url": "/tools/static/js/main.55ec0721.chunk.js"
+  },
+  {
+    "revision": "b1518d91fd0a1eb7615a",
+    "url": "/tools/static/js/runtime-main.07a40949.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
